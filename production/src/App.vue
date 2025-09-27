5d137e2f003106fd0b858a10a9526035b5e52ef1
wenom6 <kyshnihivahn@gmail.com>
2025-09-27 19:33:34 +0300

    4

--- production/src/App.vue
+++ production/src/App.vue
@@ -5,14 +5,18 @@
     description="Стильна вишиванка з орнаментами."
     price="1500"
     image="https://recommerce.com.ua/static/kozachok.reshop.com.ua/catalog/381/195715475364252e14cc6cb_medium.jpg"/>
+  <ReviewsComponent/>
   </div>
 </template>
 
 <script>
-  import ProductList from './components/ProductList.vue';
-  export default {
-    components: {
-      ProductList,
-    }
+import ProductList from "./components/ProductList.vue";
+import ReviewsComponent from "./components/ReviewsComponent.vue";
 
-};</script>
+export default {
+  components: {
+    ProductList,
+    ReviewsComponent,
+  },
+}
+</script>
---
+++ production/src/components/ReviewsComponent.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <h2>Відгуки</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Ім'я</th>
+            <th>Рейтинг</th>
+            <th>Коментар</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(review, index) in reviews" :key="index">
+            <td>{{ review.Name }}</td>
+            <td>{{ review.Rating }}</td>
+            <td>{{ review.Comment }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  import Papa from 'papaparse';
+  
+  export default {
+    data() {
+      return {
+        reviews: [], // Масив для зберігання розпарсених даних
+      };
+    },
+    created() {
+      const csvContent = `
+  Name,Rating,Comment
+Іван,5,Дякую за швидку доставку
+Ольга,5,Все супер! Буду замовляти ще
+Андрій,5,Обслуговування, як завжди - на висоті
+Марина,4,Все чудово! Покупкою задоволена, але коробка була прим'ята.
+Сергій,5,Рекомендую цей чудовий магазин усім!
+Данило,5,Дуже демократичні ціни. Я завжди купую лише тут.
+Матвій,5,Дякую за подарунок до покупки - завжди радуєте приємними сюрпризами.
+Анна,5,Дякую за чудові знижки
+Раймонд,4,Як завжди все кльово, але малий термін гарантії для годинника, хотілося б довше.
+      `;
+      Papa.parse(csvContent.trim(), {
+        header: true,
+        complete: (result) => {
+          this.reviews = result.data; // Зберігаємо розпарсені дані
+        },
+      });
+    },
+  };
+  </script>
+  
+  <style scoped>
+  h2{
+    text-align: center;
+  }
+  table {
+    width: 90%;
+    margin:30px auto;
+  }
+  th, td {
+   background: #f8f9fa;
+    padding: 4px;
+  }
+  </style>
